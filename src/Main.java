ff5b18bccd0c6afa5071b25b09066cb674b08a3f
Anatoly <itrm000@gmail.com>
2020-09-09 14:28:40 +0500

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 2_000_000_000;
+        System.out.println(balance);
+        int transfer = 500_000_000;
+        System.out.println(transfer);
+        int finelBalance = balance + transfer;
+        System.out.println(finelBalance);
+
+    }
+}
